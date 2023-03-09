507e19fa9b24e2d14a1b9b41ed5909aa64579325
osawa-koki <github@osawa.dev>
2023-03-09 12:13:26 +0900

    コード書いた。

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CommandLine;
+
+Parser.Default.ParseArguments<Options>(args).WithParsed(RunOptions);
+
+void RunOptions(Options options)
+{
+  string inputFilePath = options.InputFilePath!;
+  string outputFilePath = options.OutputFilePath!;
+  bool encode = options.Encode!;
+  bool decode = options.Decode!;
+
+  // 指定されたオプションに応じてエンコードまたはデコードを行う
+  if (encode)
+  {
+    EncodeFile(inputFilePath, outputFilePath);
+  }
+  else if (decode)
+  {
+    DecodeFile(inputFilePath, outputFilePath);
+  }
+  else
+  {
+    Console.WriteLine("Please specify --encode or --decode.");
+  }
+}
+
+void EncodeFile(string inputFilePath, string outputFilePath)
+{
+  byte[] bytes = File.ReadAllBytes(inputFilePath);
+  string base64 = Convert.ToBase64String(bytes);
+  File.WriteAllText(outputFilePath, base64);
+  Console.WriteLine("Encoded successfully.");
+}
+
+void DecodeFile(string inputFilePath, string outputFilePath)
+{
+  string base64 = File.ReadAllText(inputFilePath);
+  byte[] bytes = Convert.FromBase64String(base64);
+  File.WriteAllBytes(outputFilePath, bytes);
+  Console.WriteLine("Decoded successfully.");
+}
+
+class Options
+{
+  [Option('i', "input", Required = true, HelpText = "Input file path.")]
+  public string? InputFilePath { get; set; }
+
+  [Option('o', "output", Required = true, HelpText = "Output file path.")]
+  public string? OutputFilePath { get; set; }
+
+  [Option('e', "encode", Required = true, HelpText = "Encode input file.")]
+  public bool Encode { get; set; }
+
+  [Option('d', "decode", Required = true, HelpText = "Decode input file.")]
+  public bool Decode { get; set; }
+}
